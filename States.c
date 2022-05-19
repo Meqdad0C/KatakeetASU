0a4d43d7bccb8a27c22de22b480b9d7aeaa2b402
youssefsalahTM4C <105678947+youssefsalahTM4C@users.noreply.github.com>
2022-05-19 05:59:44 +0200

    States

---
+++ States.c
@@ -0,0 +1,166 @@
+#include "States.h"
+
+
+
+void popcorn(void){
+    state = 'A';
+    lcd_command(0x01);
+    lcd_command(0x80);
+    LCD_String("Popcorn mode");
+    delay_ms(1000);
+    lcd_command(0x01);
+    lcd_command(0x80);
+    LCD_String("Press SW1 to ");
+    lcd_command(0xC0);
+    LCD_String("start cooking");
+
+
+
+}
+
+void Beef(void){
+    state='B';
+    delay_ms(200);
+    lcd_command(0x01);
+    lcd_command(0x80);
+    LCD_String("Beef weight?");
+    char key = keypad_getkey();
+    //int c = key;
+    if(key>48 && key<58){
+        delay_ms(200);
+    lcd_command(0x01);
+    lcd_command(0x80);
+
+    lcd_data(key);
+
+    LCD_String(" Kilos");
+    delay_ms(1000);
+    lcd_command(0x01);
+    lcd_command(0x80);
+    LCD_String("Press SW1 to ");
+    lcd_command(0xC0);
+    LCD_String("start cooking");
+    secondsPerKilo = (key-48)*30;
+
+
+    }
+    else {
+    LCD_errormsg();
+    Beef();
+    }
+}
+
+void Chicken(void){
+    state='C';
+    delay_ms(200);
+    lcd_command(0x01);
+    lcd_command(0x80);
+    LCD_String("Chicken weight?");
+    char key = keypad_getkey();
+
+    if(key>48 && key<58){
+        delay_ms(200);
+    lcd_command(0x01);
+    lcd_command(0x80);
+
+    lcd_data(key);
+
+    LCD_String(" Kilos");
+    delay_ms(1000);
+    lcd_command(0x01);
+    lcd_command(0x80);
+    LCD_String("Press SW1 to ");
+    lcd_command(0xC0);
+    LCD_String("start cooking");
+    secondsPerKilo = (key-48)*12;
+
+
+    }
+    else {
+    LCD_errormsg();
+    Beef();
+    }
+}
+
+
+void D(void){
+
+    state = 'D';
+    lcd_command(0x0C);
+    delay_ms(200);
+    lcd_command(0x01);
+    lcd_command(0x80);
+    LCD_String("Cooking Time?");
+
+
+    lcd_command(0xC0);
+    LCD_String("00:00");
+
+    char cmds[4] ={0xc4,0xc3,0xc1,0xc0};
+
+    for(k=0;k<4;k++){
+
+        while(keypad_getkey()==0){}
+        if(k==-1) k=0;
+                char c = keypad_getkey();
+
+                    while(c<48||c>57){
+
+                    c = keypad_getkey();}
+        inputs[k]=c;
+
+
+
+        lcd_command(cmds[0]);
+        lcd_data(inputs[k]);
+        sD = (inputs[0]-48);
+
+
+        if(k==1){
+
+            lcd_command(cmds[1]);
+            lcd_data(inputs[k-1]);
+            sD = 10*(inputs[0]-48)+(inputs[1]-48);
+        }
+        if(k==2){
+
+        lcd_command(cmds[1]);
+        lcd_data(inputs[k-1]);
+        lcd_command(cmds[2]);
+        lcd_data(inputs[k-2]);
+
+        sD = 10*(inputs[1]-48)+(inputs[2]-48);
+        mD=(inputs[0]-48);
+                }
+
+        if(k==3){
+
+          lcd_command(cmds[1]);
+          lcd_data(inputs[k-1]);
+          lcd_command(cmds[2]);
+          lcd_data(inputs[k-2]);
+          lcd_command(cmds[3]);
+           lcd_data(inputs[k-3]);
+                sD = 10*(inputs[2]-48)+(inputs[3]-48);
+                mD= (inputs[0]-48)*10+(inputs[1]-48);
+                    if((mD*60+sD)>1800){
+                    sD=0;
+                    mD=0;
+                        for(int i=0;i<4;i++){
+            inputs[i]='0';
+                        }
+
+                        lcd_command(0x01);
+                        lcd_command(0x80);
+                        LCD_String("Max 30 min");
+                        delay_ms(1000);
+                        D();
+
+                    }
+
+                      }
+        while(k==3){}
+
+    delay_ms(500);
+    }
+}
---
+++ States.h
@@ -0,0 +1,12 @@
+#ifndef __STATES_H__
+#define __STATES_H__
+
+//#include "tm4c123gh6pm.h"
+#include "global.h"
+#include "prototype.h"
+
+#endif
+
+
+
+

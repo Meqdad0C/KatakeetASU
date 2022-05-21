1fa40f6b3ae6ed5161f1f3ba37747415ba7072a3
youssefsalahTM4C <105678947+youssefsalahTM4C@users.noreply.github.com>
2022-05-21 04:41:50 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 
 int k=0;
 int FlagH=0;
+int FlagF=0;
 char state=0;
 int flagD=0;
-int secondsPerKilo=0;
+int FlagX=0;
+//int secondsPerKilo=0;
 char inputs[4]={'0'};
-int sD=0;
-int mD=0;
+//int sD=0;
+//int mD=0;
 
 
 
@@ -19,8 +21,10 @@ int main()
     portf_init();
     portd_init();
     porta_init();
-    getStarted();
+
     while(1){
-  if(FlagH){getStarted();}
+        getStarted();
+        while(FlagH==0){}
+        FlagH=0;
     }
 }
